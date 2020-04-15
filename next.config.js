d9ee8ffe68fdd77d63c2574914158111838d9080
fashioncstar <apple@mac-dragon.local>
2020-04-15 15:01:00 +0800

    login with configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ const config = require('config')
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   /* config options here */
-  publicRuntimeConfig: config.publicRuntimeConfig,
-  auth: config.auth
+  publicRuntimeConfig: config.publicRuntimeConfig
 })
